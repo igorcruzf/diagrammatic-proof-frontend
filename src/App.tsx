3832b30113a6d5aae616fff069652f6303be8f61
igorcruzf <igfigueiredo@uolinc.com>
2022-07-21 17:58:47 -0300

    [Fix] - Tentando impedir o zoom out na versão mobile

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import React from "react";
 function App() {
 
     return <div className="content">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=no" />
         <MenuButtons/>
         <Routes>
             <Route path="/" element={<MainPage/>}/>
